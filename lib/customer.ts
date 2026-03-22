413e707ac2d3d8f169ac6c7b2ad6358bfd6c61ae
Jose Yepes <ventas@movitecgames.com>
2026-03-22 12:29:54 -0500

    fix customer cookies await

--- lib/customer.ts
+++ lib/customer.ts
@@ -1,24 +1,15 @@
+import { medusa } from "./medusa"
 import { cookies } from "next/headers"
 
 export async function getCustomer() {
   try {
-    const token = cookies().get("_medusa_jwt")?.value
+    const cookieStore = await cookies()
+    const token = cookieStore.get("_medusa_jwt")?.value
 
     if (!token) return null
 
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL}/store/customers/me`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        cache: "no-store",
-      }
-    )
+    const { customer } = await medusa.store.customer.retrieve()
 
-    if (!res.ok) return null
-
-    const { customer } = await res.json()
     return customer
   } catch (error) {
     return null
