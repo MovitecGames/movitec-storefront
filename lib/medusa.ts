ae51b23079b5a2a35b36d1dd2613d0d2424395da
Jose Yepes <ventas@movitecgames.com>
2026-03-23 20:20:12 -0500

    fix login session auth medusa

--- lib/medusa.ts
+++ lib/medusa.ts
@@ -3,7 +3,9 @@ import Medusa from "@medusajs/js-sdk"
 export const medusa = new Medusa({
   baseUrl: process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL!,
   publishableKey: process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY!,
+  debug: process.env.NODE_ENV === "development",
   auth: {
     type: "session",
+    fetchCredentials: "include",
   },
 })
