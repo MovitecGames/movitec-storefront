eca7acb3a08cea7cd944fb0c00792d118fcf44c3
Jose Yepes <ventas@movitecgames.com>
2026-03-26 06:06:30 -0500

    fix customer session detection

--- lib/customer.ts
+++ lib/customer.ts
@@ -1,15 +1,8 @@
 import { medusa } from "./medusa"
-import { cookies } from "next/headers"
 
 export async function getCustomer() {
   try {
-    const cookieStore = await cookies()
-    const token = cookieStore.get("_medusa_jwt")?.value
-
-    if (!token) return null
-
     const { customer } = await medusa.store.customer.retrieve()
-
     return customer
   } catch (error) {
     return null
